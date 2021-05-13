6188ecbb2c1fc30fae16cce72db9aa574f52e7e4
rschpdr <pedro.resch@gmail.com>
2021-05-13 18:11:54 -0300

    added stripe integration

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -3,8 +3,13 @@ const TransactionModel = require("../models/Transaction.model");
 const UserModel = require("../models/User.model");
 const ProductModel = require("../models/Product.model");
 
-// Criar uma nova transação (compra)
-router.post("/transaction", async (req, res) => {
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+// Criar uma sessão de Checkout no Stripe
+router.post("/create-checkout-session", async (req, res) => {
+  // Array para segurar dados dos produtos
+  const line_items = [];
+
   try {
     // Antes de liberar a venda, verifica se tem quantidade em estoque
     for (let product of req.body.products) {
@@ -15,8 +20,39 @@ router.post("/transaction", async (req, res) => {
       if (product.qtt > foundProduct.qtt_in_stock) {
         return res.status(403).json({ msg: "Not enough quantity in stock" });
       }
+
+      // Esse formato de objeto é o formato requerido pela API do Stripe
+      line_items.push({
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: foundProduct.name,
+            images: [foundProduct.image_url],
+          },
+          unit_amount: parseInt(foundProduct.price * 100),
+        },
+        quantity: product.qtt,
+      });
     }
 
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [...line_items],
+      mode: "payment",
+      success_url: `${process.env.REACT_APP_URL}/order/success`,
+      cancel_url: `${process.env.REACT_APP_URL}/order/canceled`,
+    });
+
+    return res.status(201).json({ id: session.id });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ msg: "Failed to create checkout session." });
+  }
+});
+
+// Criar uma nova transação (compra)
+router.post("/transaction", async (req, res) => {
+  try {
     // Criar a transação
     const result = await TransactionModel.create(req.body);
 
